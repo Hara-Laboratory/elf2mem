10159d04ae5723ce8dbd3f59e627f7088368e6d3
na4zagin3 <mrty.ityt.pt@gmail.com>
2015-01-31 02:12:05 +0900

    add default values of output file and name

--- elf2mem.cpp
+++ elf2mem.cpp
@@ -36,17 +36,17 @@ void printusage(const char *name, std::ostream &ost) {
 	ost << "\t-E\tOutput until end of the sections. (default)" << std::endl;
 	ost << "\t-t <type>\tOutput type." << std::endl;
 	ost << "\t\t\tAvailable choices: 'c-array' (default)" << std::endl;
-	ost << "\t-o <filename>\tOutput file name (scheme)." << std::endl;
+	ost << "\t-o <filename>\tOutput file name (scheme). (default: basename of input file + \".h\")" << std::endl;
 	ost << "\t-s <num>\tSplit number. (default: 1)" << std::endl;
 	ost << std::endl;
 	ost << "for output type 'c-array':" << std::endl;
-	ost << "\t-n <name>\tIdentifier name." << std::endl;
+	ost << "\t-n <name>\tIdentifier name. (default: basename of input file)" << std::endl;
 }
 
 int main (int argc, char **argv) {
 
 	const char *input_file = NULL;
-	const char *output_file = NULL;
+	std::string *output_file = nullptr;
 	const char *output_name = NULL;
 	bool begin_address_set = false;
 	size_t begin_address;
@@ -65,12 +65,11 @@ int main (int argc, char **argv) {
 				}
 				break;
 			case 'o':
-				output_file = optarg;
+				output_file = new std::string(optarg);
 				break;
 			case 'b':
 			{
-				std::istringstream istr(optarg);
-				istr >> begin_address;
+				begin_address = std::stoull(optarg, nullptr, 0);
 				begin_address_set = true;
 				break;
 			}
@@ -79,8 +78,7 @@ int main (int argc, char **argv) {
 				break;
 			case 'e':
 			{
-				std::istringstream istr(optarg);
-				istr >> end_address;
+				end_address = std::stoull(optarg, nullptr, 0);
 				end_address_set = true;
 				break;
 			}
@@ -117,6 +115,18 @@ int main (int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
+	if (output_file == nullptr) {
+		std::ostringstream ostr;
+		ostr << basename(input_file) << ".h";
+		output_file = new std::string(ostr.str());
+	}
+	std::cout << "output: " << output_file << std::endl;
+
+	if (output_name == NULL) {
+		output_name = basename(input_file);
+	}
+	std::cout << "identifier: " << output_name << std::endl;
+
 	FILE *fp = fopen(input_file, "r");
 	if (fp == NULL){
 		printf("Can't open elf_file\n");
@@ -139,7 +149,7 @@ int main (int argc, char **argv) {
 	}
 	*/
 
-	std::ofstream ofs(output_file);
+	std::ofstream ofs(output_file->c_str());
 	printerC printerc(output_name, begin_address_set, begin_address, end_address_set, end_address, split);
 	printerc.print_mem(ofs, mem);
 
